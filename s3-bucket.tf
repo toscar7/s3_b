f9fdbb9c12d1b314f760b1f6e07224ee9008eb35
toscar <toscar7@yahoo.co.uk>
2021-12-23 21:10:19 -0800

    s3

---
+++ s3-bucket.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-2"
+
+}
+
+
+resource "aws_s3_bucket" "onebucket" {
+    bucket         = "demo-s3-bucket-use-terraform"
+
+        tags = {
+	      Name = "s3-bucket"
+    }  
+}
+
