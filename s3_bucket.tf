a2a7ae9a9a606f20d125fdafe07216545888de4f
toscar <toscar7@yahoo.co.uk>
2021-12-23 21:29:31 -0800

    adjust

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,14 +1,13 @@
 provider "aws" {
     region = "us-east-2"
-
 }
 
 
 resource "aws_s3_bucket" "onebucket" {
-    bucket         = "demo-s3-bucket-use-terraform"
+    bucket   = "demo-s3-bucket-use-terraform"
 
         tags = {
 	      Name = "s3-bucket"
+		}
     }  
 }
-
