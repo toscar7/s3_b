dfcbdd99f0eeca40ced0deaec02b6ade2de9088a
toscar <toscar7@yahoo.co.uk>
2021-12-23 21:35:18 -0800

    adjustment

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -4,10 +4,5 @@ provider "aws" {
 
 
 resource "aws_s3_bucket" "onebucket" {
-    bucket   = "demo-s3-bucket-use-terraform"
-
-        tags = {
-	      Name = "s3-bucket"
-		}
-    }  
+    bucket   = "demo-s3-bucket-use-terraform"  
 }
